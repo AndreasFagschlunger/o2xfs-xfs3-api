e3ac7544d00f6315cc32e7b04fa4ec53444408f5
AndreasFagschlunger <github@fagschlunger.co.at>
2019-11-03 22:10:18 +0100

    Added Calibrate3Test.java

---
+++ src/test/java/at/o2xfs/xfs/v3/cdm/Calibrate3Test.java
@@ -0,0 +1,21 @@
+package at.o2xfs.xfs.v3.cdm;
+
+import java.util.Optional;
+
+import at.o2xfs.xfs.cdm.RetractArea;
+import at.o2xfs.xfs.v3.BaseTest;
+
+class Calibrate3Test extends BaseTest {
+
+	private Calibrate3 calibrate = new Calibrate3.Builder().number(1).numOfBills(3)
+			.position(new Itemposition3.Builder().number(1)
+					.retractArea(
+							Optional.of(new Retract3.Builder().retractArea(RetractArea.ITEMCASSETTE).index(1).build()))
+					.build())
+			.build();
+
+	@Override
+	public Object getValue() {
+		return calibrate;
+	}
+}
