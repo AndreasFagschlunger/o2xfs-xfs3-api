c99fbf846fcdfeec2b2e827eedb5d6864e89ae20
AndreasFagschlunger <github@fagschlunger.co.at>
2020-02-09 22:51:28 +0100

    Itemposition3: com.fasterxml.jackson.*

--- src/main/java/at/o2xfs/xfs/v3/cdm/Itemposition3.java
+++ src/main/java/at/o2xfs/xfs/v3/cdm/Itemposition3.java
@@ -6,19 +6,14 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
-
 import at.o2xfs.memory.databind.annotation.MemoryPropertyOrder;
 import at.o2xfs.memory.databind.annotation.win32.UShort;
 import at.o2xfs.xfs.cdm.Position;
 import at.o2xfs.xfs.databind.annotation.XfsEnum16;
 
 @MemoryPropertyOrder({ "number", "retractArea", "outputPosition" })
-@JsonDeserialize(builder = Itemposition3.Builder.class)
 public class Itemposition3 {
 
-	@JsonPOJOBuilder(withPrefix = "")
 	public static class Builder {
 
 		private int number;
